320d410182e5b4bb256fcbe11aa4d2baa92a80be
Johan Selim Fendi <johanfendi0112@gmail.com>
2025-04-13 21:38:34 +0200

    Impelemntet datasetReader reader

--- abstractBaseClasses/DBReader.py
+++ abstractBaseClasses/DBReader.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
+from typing import Dict, Any
 
 class DBReader(ABC): 
 
-    """Returns points from database"""
+    """
+    Retrieve specified key-value pairs from a data point at the given index.
+    """
     @abstractmethod
-    def getPoint(index:int): 
+    def getPoint(index:int,  keys : list[str]) -> Dict[str, Any]: 
         pass
--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import ollama
-from OllamaPrompters import OllamaPrompter
 from datasets import load_dataset
 
-model_name =  "llama3.2-vision:latest"
-prompter = OllamaPrompter(ollama.Client(), model_name)
-task_description = """Your task is to answer the following coding 
-                        question in haskell. You should not write any comments,
-                        not include any natural language in your respons and not use 
-                        any imports.
-                    """
-coding_problem = "Write a method that takes in a integer and returns its square root. "
+from OllamaPrompters import OllamaPrompter
+from datasetReaders import DatasetReader
 
 
 dataset_name = "deepmind/code_contests"
 dataset = load_dataset(dataset_name)
 
-print(dataset["train"][13])
+keys = ["description", "public_tests", "private_tests", "generated_tests"]
+
+reader = DatasetReader(dataset["train"])
+vals = reader.getPoint(105, keys)
+
+def printProblem(point, keys): 
+    for key in keys: 
+        print(point[key])
+
+"""
+num_null = 0
+for i in range(len(dataset["train"])): 
+    point = dataset["train"][i]
+    if len(point["private_tests"])==0 and len(point["generated_tests"])==0: 
+        num_null += 1
+
+print(num_null)
+"""
+
+l = 0
+for i in range(len(dataset["train"])): 
+    point = dataset["train"][i]
+
+    if (not point["private_tests"]["input"] and not point["private_tests"]["output"]) and (not point["generated_tests"]["input"] and not point["generated_tests"]["output"]): 
+        l += 1
+
+print(l)
